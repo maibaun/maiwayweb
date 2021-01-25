775080dee6d0031eb5bb82bb6bea82f1f77e1ffa
celmar <celmargalindez@gmail.com>
2021-01-25 14:53:03 +0800

    first commit

---
+++ gh_pages.js
@@ -0,0 +1,15 @@
+var ghpages = require("gh-pages");
+ghpages.publish(
+  "build",
+  {
+    branch: "master",
+    repo: "https://github.com/maibaun/maiway-web/",
+  },
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("success");
+    }
+  }
+);
